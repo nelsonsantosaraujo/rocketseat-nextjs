380ae3de211278d45a3e81d9a3f9c04ae36646b5
Nelson Araujo <nelsonsantosaraujo@hotmail.com>
2021-01-24 09:19:29 -0300

    Pagina de produto

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,30 +1,57 @@
 import { useRouter } from 'next/router'
-import dynamic from 'next/dynamic'
-import { useState } from 'react';
+import { client } from '@/lib/prismic';
+import Prismic from 'prismic-javascript';
+import { Document } from 'prismic-javascript/types/documents';
+import PrismicDOM from 'prismic-dom';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
-const AddToCartModal = dynamic(
-  () => import('@/components/AddToCartModal'),
-  {
-    loading: () => <p>Carregando...</p>,
-    ssr: false,
-  }
-);
+interface IProductProps {
+  product: Document;
+}
 
-export default function Products() {
+
+export default function Products({ product }: IProductProps) {
   const router = useRouter();
-  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
-  function handleAddToCart(){
-    setIsAddToCartModalVisible(true);
+  if(router.isFallback) {
+    return <p>Carregando...</p>
   }
 
   return (
     <div>
-      <h1>{router.query.slug}</h1>
+      <h1>
+        {PrismicDOM.RichText.asText(product.data.title)}
+      </h1>
 
-      <button onClick={handleAddToCart}>Add to cart</button>
+      <img src={product.data.thumbnail.url} width="600" alt="" />
+     
+      <div dangerouslySetInnerHTML={{__html: PrismicDOM.RichText.asHtml(product.data.description)}}></div>
 
-      {isAddToCartModalVisible && <AddToCartModal />}
+      <p>
+        Price: {product.data.price}
+      </p>
     </div>
   )
+}
+
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  }
+}
+
+
+export const getStaticProps: GetStaticProps<IProductProps> = async (context) => {
+  const { slug } = context.params;
+
+  const product = await client().getByUID('product', String(slug), {});
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 5,
+  }
 }
